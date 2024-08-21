3b57aa8bfc51f129f9333b96ff29ddab5d8fbefe
Rhys Powell <ascendingleo82@gmail.com>
2024-08-21 19:16:07 +1200

    Add postData fetch method to DataService and add logic to create new routine from Routines page

--- frontend/src/DataService.ts
+++ frontend/src/DataService.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL = await import.meta.env.VITE_PROXY;
 
-export const fetchData = async (endpoint: string) => {
+export const getData = async (endpoint: string) => {
   try {
     const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
       method: 'GET',
@@ -9,9 +9,23 @@ export const fetchData = async (endpoint: string) => {
       },
     });
 
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const postData = async (endpoint: string, payload: object) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
 
     const data = await response.json();
     return data;
--- frontend/src/pages/Routines.tsx
+++ frontend/src/pages/Routines.tsx
@@ -1,29 +1,80 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthHooks';
-import { fetchData } from '../DataService';
+import { getData, postData } from '../DataService';
 import { Routine } from '../types/Routine';
 
 export default function Routines() {
   const [routines, setRoutines] = useState<Routine[]>([]);
   const auth = useAuth();
+  const [input, setInput] = useState({ name: '' });
+  const userId = auth?.user?.id;
+  const [createMode, setCreateMode] = useState(false);
 
   const getRoutines = useCallback(async () => {
-    const data: Routine[] = await fetchData('users/' + auth?.user?.id + '/routines');
-    console.log(data);
-    return data;
-  }, [auth?.user?.id]);
+    try {
+      const data: Routine[] = await getData('users/' + userId + '/routines');
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }, [userId]);
+
+  async function createRoutine() {
+    if (userId != null) {
+      try {
+        await postData('users/' + userId + '/routines', { userId: userId, name: input.name });
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.error('User ID is null');
+      setCreateMode(false);
+    }
+  }
 
   useEffect(() => {
-    if (auth) {
-      getRoutines().then((data) => setRoutines(data));
+    if (auth && userId) {
+      getRoutines().then((value) => setRoutines(value ?? []));
     }
-  }, [auth, getRoutines]);
+  }, [auth, userId, getRoutines]);
+
+  function handleClick() {
+    setCreateMode(true);
+  }
+
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setInput((prevInput) => ({ ...prevInput, [name]: value }));
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (input.name !== '') {
+      createRoutine();
+    }
+  }
 
   return (
-    <ul>
-      {routines.map((routine) => (
-        <li key={routine.id}>{routine.name}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {routines.map((routine) => (
+          <li key={routine.id}>{routine.name}</li>
+        ))}
+      </ul>
+      {createMode ? null : <button onClick={handleClick}>Add Routine</button>}
+
+      {createMode ? (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Routine Name:
+            <input type="string" name="name" onChange={handleInput} />
+          </label>
+          <button type="submit">Submit</button>
+          <button type="button" onClick={() => setCreateMode(false)}>
+            Cancel
+          </button>
+        </form>
+      ) : null}
+    </>
   );
 }
