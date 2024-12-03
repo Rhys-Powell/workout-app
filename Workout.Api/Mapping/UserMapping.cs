526ba7ec214187da52aeb7b4b36b4a554f5a7625
Rhys Powell <ascendingleo82@gmail.com>
2024-12-03 18:50:20 +1300

    Remove 'Auth|' prefix in toEntity method in UserMapping

--- Workout.Api/Mapping/UserMapping.cs
+++ Workout.Api/Mapping/UserMapping.cs
@@ -24,7 +24,7 @@ public static class UserMapping
             Id = user.Id,
             Name = user.Name,
             Email = user.Email,
-            Auth0Id = user.Auth0Id
+            Auth0Id = StringHelper.ExtractSubstring(user.Auth0Id, "|")
         };
     }
 }
